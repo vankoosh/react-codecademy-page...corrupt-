58b526c1acf510a6cf66a29a526d6d069155347a
vankoosh <vankoosh@users.noreply.github.com>
2022-04-23 19:11:30 -0100

    navbar started

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Work</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
