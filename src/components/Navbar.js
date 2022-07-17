a05d3e3a15b11cdb5f9ab44567d18e112209fbc3
vankoosh <vankoosh@users.noreply.github.com>
2022-07-17 21:16:44 -0100

    navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ export default function Navbar() {
     <nav>
       <div className="navbar">
         <ul>
-          <li>Loops</li>
-          <li>Conditionals</li>
-          <li>Iterators</li>
-          <li>Array</li>
-          <li>Functions</li>
-          <li>Objects</li>
-          <li>Classes</li>
-          <li>XHR</li>
-          <li>Promises</li>
-          <li>Async Await</li>
-          <li>Modules</li>
+          <li><a href="#">Loops</a></li>
+          <li><a href="#">Conditionals</a></li>
+          <li><a href="#">Iterators</a></li>
+          <li><a href="#">Array</a></li>
+          <li><a href="#">Functions</a></li>
+          <li><a href="#">Objects</a></li>
+          <li><a href="#">Classes</a></li>
+          <li><a href="#">XHR</a></li>
+          <li><a href="#">Promises</a></li>
+          <li><a href="#">Async Await</a></li>
+          <li><a href="#">Modules</a></li>
         </ul>
       </div>
     </nav>
