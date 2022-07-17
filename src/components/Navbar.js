29b06677524eb82ec6f868fbb510797824d925ef
vankoosh <vankoosh@users.noreply.github.com>
2022-07-17 21:11:17 -0100

    started navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,22 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li>Home</li>
-        <li>Work</li>
-        <li>About me</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <nav>
+      <div className="navbar">
+        <ul>
+          <li>Loops</li>
+          <li>Conditionals</li>
+          <li>Iterators</li>
+          <li>Array</li>
+          <li>Functions</li>
+          <li>Objects</li>
+          <li>Classes</li>
+          <li>XHR</li>
+          <li>Promises</li>
+          <li>Async Await</li>
+          <li>Modules</li>
+        </ul>
+      </div>
+    </nav>
   );
 }
